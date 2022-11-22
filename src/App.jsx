d1a165ccbd49f2141032a66c962c46d0bb29fa86
Amanda Freitas <amandaafreitas20@gmail.com>
2022-11-22 15:50:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import reactLogo from './assets/react.svg'
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Bem vindo ao Cine cine</h1>
+    </>
+  );
+}
+
+export default App;
