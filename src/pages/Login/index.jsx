55f37ad89811241a2a0384ed5a942e40fc7b91af
Amanda Freitas <amandaafreitas20@gmail.com>
2022-11-24 14:55:05 -0300

    validacao funcionando

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,41 +1,50 @@
 import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const validacaoSchema = yup.object({
+  nome: yup.string().required("O campo nome não pode ser vazio"),
+  senha: yup
+    .string()
+    .min(4, "A senha deve ter no mínimo 4 caracteres")
+    .max(10, "A senha deve ter no máximo 10 caracteres")
+    .required("O campo senha não pode ser vazio"),
+});
 
 const Login = () => {
-  const validacaoSchema = yup.object({
-    nome: yup.string().required(),
-    senha: yup.string().min(4).max(10).required(),
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validacaoSchema), //connecting yup with react-hook-form
   });
 
-  const valida = async (e) => {
-    e.preventDefault();
-    let formData = {
-      nome: e.target[0].value,
-      senha: e.target[1].value,
-    };
-
-    const estahValido = await validacaoSchema.isValid(formData);
-    console.log(estahValido);
+  const valida = (data) => {
+    console.log(data);
   };
 
   return (
     <>
-      <form onSubmit={valida}>
+      <form onSubmit={handleSubmit(valida)}>
         <label htmlFor="nome">Nome</label>
         <input
           type="text"
           id="nome"
           name="nome"
           placeholder="Nome"
-          autoComplete="on"
+          {...register("nome")}
         />
+        <p>{errors.nome?.message}</p>
         <label htmlFor="senha">Senha</label>
         <input
           type="password"
           id="senha"
           name="senha"
           placeholder="Senha"
-          autoComplete="on"
+          {...register("senha")}
         />
+        <p>{errors.senha?.message}</p>
         <button type="submit">Entrar</button>
       </form>
     </>
